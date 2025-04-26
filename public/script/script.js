04313cb7b0edde80fa3d9163333abe8f1615c364
luuk kessels <lmpkessels@gmail.com>
2025-04-26 16:35:13 +0200

    Add mobile and tablet size

--- public/script/script.js
+++ public/script/script.js
@@ -1,17 +1,36 @@
-// Theme toggle logic
 const body = document.body;
-
-// Check theme user
-if (localStorage.getItem("theme") === "default") {
-  body.classList.add("default-mode");
-} else {
-  body.classList.remove("default-mode");
-}
-
-document.addEventListener("click", (e) => {
-  if (e.target.closest(".toggle-wrapper")) {
-    body.classList.toggle("default-mode");
-    const isDefault = body.classList.contains("default-mode");
-    localStorage.setItem("theme", isDefault ? "default" : "dark");
-  }
-});
+"default" === localStorage.getItem("theme")
+  ? body.classList.add("default-mode")
+  : body.classList.remove("default-mode"),
+  document.addEventListener("click", (e) => {
+    if (e.target.closest(".toggle-wrapper")) {
+      body.classList.toggle("default-mode");
+      let t = body.classList.contains("default-mode");
+      localStorage.setItem("theme", t ? "default" : "dark");
+    }
+  }),
+  fetch("partials/header.html")
+    .then((e) => e.text())
+    .then((e) => {
+      document.getElementById("header").innerHTML = e;
+    }),
+  fetch("partials/toggle.html")
+    .then((e) => e.text())
+    .then((e) => {
+      document.getElementById("li-da-mode-toggle").innerHTML = e;
+    }),
+  fetch("partials/socials.html")
+    .then((e) => e.text())
+    .then((e) => {
+      document.getElementById("socials").innerHTML = e;
+    }),
+  fetch("partials/subjects.html")
+    .then((e) => e.text())
+    .then((e) => {
+      document.getElementById("subjects").innerHTML = e;
+    }),
+  fetch("template/post-lead.html")
+    .then((e) => e.text())
+    .then((e) => {
+      document.getElementById("lead-post-one").innerHTML = e;
+    });
