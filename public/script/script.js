1ecf9de9a3f80c4ae8856734b48cb6e6c05f9c15
luuk kessels <lmpkessels@gmail.com>
2025-05-04 15:16:54 +0200

    chore: add .gitignore, comment CSS, remove unused

--- public/script/script.js
+++ public/script/script.js
@@ -1,26 +1,28 @@
 // Toggle mode + check default mode user
-document.addEventListener("DOMContentLoaded", () => {
-  let e = document.body;
-  "default" === localStorage.getItem("theme")
-    ? e.classList.add("default-mode")
-    : e.classList.remove("default-mode"),
-    document.addEventListener("click", (t) => {
-      if (t.target.closest(".toggle-wrapper")) {
-        e.classList.toggle("default-mode");
-        let l = e.classList.contains("default-mode") ? "default" : "dark";
-        localStorage.setItem("theme", l);
-      }
-    }),
-    /* 
-    Fetching parts out of html files stored in folders, to implement in main.
-    
-    Notes with line of code are for -> 1st = developer mode, 
-    2nd = production mode.
-    */
-    fetch("../partials/toggle.html")
-      // fetch("/partials/toggle.html")
-      .then((e) => e.text())
-      .then((e) => {
-        document.querySelector(".mode-toggle").innerHTML = e;
-      });
-});
+let e = document.body;
+/* 
+  Check local storage user if theme is light add default-mode, 
+  else remove default-mode 
+  */
+"default" === localStorage.getItem("theme")
+  ? e.classList.add("default-mode")
+  : e.classList.remove("default-mode"),
+  // Listen to click of toggle and do the following within the funtion
+  document.addEventListener("click", (t) => {
+    // When toggle is clicked, change theme to default mode
+    if (t.target.closest(".toggle-wrapper")) {
+      e.classList.toggle("default-mode");
+      // Check body contains default, if so set to default, else dark
+      let l = e.classList.contains("default-mode") ? "default" : "dark";
+      // Set local storage to chosen theme
+      localStorage.setItem("theme", l);
+    }
+  });
+//     /*Toggle from */
+//     fetch("../partials/toggle.html")
+//       // fetch("/partials/toggle.html")
+//       .then((e) => e.text())
+//       .then((e) => {
+//         document.querySelector(".mode-toggle").innerHTML = e;
+//       });
+// })
