f6765534464fb41087ae0ae1f290e24b7e7485c3
luuk kessels <lmpkessels@gmail.com>
2025-05-09 15:11:16 +0200

    add: fetch for production && development

--- script/toggle.js
+++ script/toggle.js
@@ -6,7 +6,8 @@ if (localStorage.getItem("theme") === "default") {
 }
 
 // Fetch and insert the toggle from partials
-fetch("partials/toggle.html")
+// fetch("/toggle.html")
+fetch("/partials/toggle.html")
   .then((res) => res.text())
   .then((htmlContent) => {
     const container = document.querySelector(".light-dark-toggle");
